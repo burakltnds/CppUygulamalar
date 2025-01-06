98dbcdb7b71a7ff1ebac12f1600ec5d46c2276b1
Burak Altundaş <156445457+burakltnds@users.noreply.github.com>
2025-01-06 12:41:10 +0300

    Add files via upload
    Pointer Referans ve Copynin Nerede Tercih Edileceğine Dair Örnek

---
+++ PointerReferanceCopy.cpp
@@ -0,0 +1,51 @@
+﻿#include <iostream>
+using namespace std;
+
+//kopya ile yer değiştirme
+void swap(int x, int y) {
+	int temp;
+	temp = x;
+	x = y;
+	y = temp;
+	cout << "Kopya Icinde Degerler:" << "x=" << x << " y=" << y << endl;
+}
+//Referans ile yer değiştirme 
+void swap2(int &x,int &y) {
+	int temp;
+	temp = x;
+	x = y;
+	y = temp;
+	cout << "Referans Icinde Degerler:" << "z=" << x << " t=" << y << endl;
+}
+//Pointer ile yer değiştirme 
+void swap3(int* x, int* y) {
+	int temp;
+	temp = *x;
+	*x = *y;
+	*y = temp;
+	cout << "Pointer Icinde Degerler:" << "a=" << *x << " b=" << *y << endl;
+}
+
+int main()
+{
+	int x = 5, y = 10, z = 20, t = 40;
+	int h = 50, m = 100;
+	int* a = &h;
+	int* b = &m;
+	//Kopya
+	cout << "Kopya Fonksiyon Oncesi degerler:" << "x="<<x<<" y="<<y << endl;
+	swap(x, y);
+	cout << "Kopya Fonksiyon Sonrasi degerler:" << "x=" << x << " y=" << y << endl;
+	cout << "**********" << endl;
+	//Referans
+	cout << "Referans Fonksiyon Oncesi degerler:" << "z=" << z << " t=" << t << endl;
+	swap2(z, t);
+	cout << "Referans Fonksiyon Sonrasi degerler : " << "z=" << z << " t=" << t << endl;
+	cout << "**********" << endl;
+	//Pointer
+	cout << "Pointer Fonksiyon Oncesi degerler:" << "a=" << h << " b=" << m << endl;
+	swap3(a,b);
+	cout << "Pointer Fonksiyon Sonrasi degerler:" << "a=" << h << " b=" << m << endl;
+	return 0;
+
+}
